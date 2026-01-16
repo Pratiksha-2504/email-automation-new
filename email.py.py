2449ee9a76ea63cb594a15d9cb085ccf1bccb87d
Pratiksha-2504 <panchalpratiksha309@gmail.com>
2026-01-16 10:10:26 +0530

    Updated email.py.py

--- email.py.py
+++ email.py.py
@@ -219,7 +219,7 @@ These are a few of the notebooks related to Machine Learning, including Google's
 - [Image Classification with KerasHub](https://keras.io/keras_hub/guides/classification_with_keras_hub/)
 """
 
-!pip install streamlit
+###!pip install streamlit
 import streamlit as st
 import smtplib
 from email.mime.text import MIMEText  #Multipurpose Internet Mail Extension (MIME) Protocol
